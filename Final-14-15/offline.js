8ccd0b5f7e87b07c5dea9ae6b9ea9d93c02f1939
Earth15-gg <s40660@bj.ac.th>
2025-02-25 16:58:58 +0700

    Add files via upload

---
+++ Final-14-15/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1740477372,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/bin-sheet0.png",
+		"images/item-sheet0.png",
+		"images/item-sheet1.png",
+		"images/item-sheet2.png",
+		"images/bin2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
